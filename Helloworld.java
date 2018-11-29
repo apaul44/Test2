0d4505f258889e676adbeefaeb45689d88688338
Pau <37530554+apaul44@users.noreply.github.com>
2018-11-28 19:10:34 -0500

    Update Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -1,6 +1,6 @@
 public class Helloworld {
     public static void main( String[] args ) {
-        System.out.prin( "Hello world! Hey hey how are y'all doing?" );
+        System.out.println( "Hello world! Hey hey how are y'all doing?" );
         System.exit( 0 ); //success
     }
 }
